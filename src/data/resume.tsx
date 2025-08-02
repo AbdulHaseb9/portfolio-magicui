2502a27e1a26b3be9f3028eb930585d0a651f507
AbdulHaseb9 <haseebsheikh2380@gmail.com>
2025-08-02 21:56:02 +0500

    Refactor project structure for improved organization and maintainability

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -130,7 +130,6 @@ export const DATA = {
     {
       title: "Chat Collect",
       href: "https://chatcollect.com",
-      dates: "Jan 2024 - Feb 2024",
       active: true,
       description:
         "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
